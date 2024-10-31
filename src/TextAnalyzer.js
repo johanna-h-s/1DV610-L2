241728289050199d0931cad8a352ee960604a6af
johanna-h-s <js226rv@student.lnu.se>
2024-10-31 22:30:47 +0100

    Add class TextAnalyzer

---
+++ src/TextAnalyzer.js
@@ -0,0 +1,43 @@
+/**
+ * Module for the class TextAnalyzer.
+ *
+ * @author Johanna Stenbeck <js226rv@student.lnu.se>
+ * @version 1.0.0
+ */
+
+// import { WordAnalyzer } from './helpers/WordAnalyzer.js'
+
+/**
+ * Represents a text analyzer.
+ */
+export class TextAnalyzer {
+  #stringToAnalyze
+
+  /**
+   * Initializes a new instances of the TextAnalyzer class.
+   *
+   * @param {string} stringToAnalyze - The string to analyze.
+   */
+  constructor (stringToAnalyze) {
+    this.#setStringToAnalyze(stringToAnalyze)
+  }
+
+  /**
+   * Sets the stringToAnalyze.
+   *
+   * @param {string} stringToAnalyze – The string to set.
+   */
+  #setStringToAnalyze (stringToAnalyze) {
+    if (typeof stringToAnalyze === 'string') {
+      this.#stringToAnalyze = stringToAnalyze
+
+      console.log('Text analyzer ready to use.')
+    } else {
+      console.log('The text analyzer can only analyze strings. Please provide a string.')
+
+      throw new Error('The text analyzer can only analyze strings. Please provide a string.')
+    }
+
+    console.log(this.#stringToAnalyze)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@
  * @version 1.1.0
  */
 
-import { WordAnalyzer } from './helpers/WordAnalyzer.js'
+import { TextAnalyzer } from './TextAnalyzer.js'
 
 const textToAnalyze = "Your thought, Fantasizing on a sodden brain, Like a bloated lackey on a greasy couch sprawling, With my heart's bloody tatters, I'll mock it again. Until I'm contempt, I'll be ruthless and galling."
+const numberToTry = 4
 
-const wordAnalyzer = new WordAnalyzer(textToAnalyze)
-
-console.log(wordAnalyzer.allWords)
-console.log(wordAnalyzer.uniqueWordsFreqenzy)
-console.log(wordAnalyzer.uniqueWordsLength)
+const textAnalyzer = new TextAnalyzer(textToAnalyze)
